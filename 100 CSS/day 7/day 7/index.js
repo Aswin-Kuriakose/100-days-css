f5325c837e6cdc78580cda74be36babd93786782
Aswin-Kuriakose <112945895+Aswin-Kuriakose@users.noreply.github.com>
2022-12-12 17:19:47 +0530

    js indo

---
+++ 100 CSS/day 7/day 7/index.js
@@ -0,0 +1,6 @@
+let visibility = "visible"
+let popEl = document.getElementById('pop')
+let meaningEl = document.getElementById('meaning')
+function popUp() {
+meaningEl.style.visibility = visible
+}
